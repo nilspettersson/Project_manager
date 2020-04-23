93ae0f47196268dc8ab1a4886331963535238846
nils <nils>
2020-04-23 23:05:04 +0200

    removed form and added ajax call in js. added page reload when adding projects

--- TaskManager/Scripts/js/index.js
+++ TaskManager/Scripts/js/index.js
@@ -1,13 +1,26 @@
 ﻿$(function () {
     console.log("hello");
-
     $("#save").click(function () {
-        console.log("hello there");
+        //console.log($("#name").val());
+        //console.log($("#description").val());
+        $.ajax({
+            url: "https://localhost:44373/ProjectManager/CreateProject",
+            type: "POST",
+            data: {
+                name: $("#name").val(),
+                description: $("#description").val()
+            },
+            success: function (result) {
+                //console.log(result);
+                location.reload(false);
+            }
+        });
+
+
+
     });
-
-
 
 });
 
 
-
+//@using(Ajax.BeginForm("CreateProject", "ProjectManager", new AjaxOptions{ HttpMethod = "POST" }))
