d249b5e9a9115b254011c0b90c6e0497f766855a
nils-pettersson <nils-pettsson@outlook.com>
2020-05-14 12:16:59 +0200

    added drag and drop frontend

--- TaskManager/Scripts/js/sprint.js
+++ TaskManager/Scripts/js/sprint.js
@@ -1,1 +1,13 @@
-﻿
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, el) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    el.appendChild(document.getElementById(data));
+}
